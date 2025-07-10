883c38410588a3c47d512f40b5e944c597a33678
Madhav Sanjay Patil <madhavsanjaypatil@Madhavs-MacBook-Pro.local>
2025-07-10 18:30:40 +0530

    initial commit

---
+++ static/components/Footer.js
@@ -0,0 +1,10 @@
+export default {
+    template: `
+    <div class="row border">
+        <div class="col">
+            <div class="text-center mt-2">
+                <p> Fast logistics |Created by Madhav Sanjay Patil </p>
+            </div>
+        </div>
+    </div>`
+}
